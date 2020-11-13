509a9ee2e66a218ad848eee129c20b2e0224367b
Paolo Cetti <cettipao@live.com.ar>
2020-11-13 15:04:02 -0300

    Template Integrado, pequeños cambios

--- config/settings.py
+++ config/settings.py
@@ -121,8 +121,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 
